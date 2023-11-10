da9aaf4722fdd0ca4561d7e8320a3ecdafd21de0
Anthony <93596000+AnthonyHartog@users.noreply.github.com>
2023-11-10 11:45:48 +0100

    Vragenlijst dashboard
    Je kan alle vragenlijsten zien.
    
    (Geen CSS toegevoegd)

--- resources/views/questionnaire/index.blade.php
+++ resources/views/questionnaire/index.blade.php
@@ -1,1 +1,14 @@
-<h1>Test</h1>
+<x-app-layout>
+    <table>
+        <tr>
+            <th>Naam</th>
+            <th>Aantal keer ingevuld</th>
+        </tr>
+        <tr>
+            @foreach ($questionnaires as $questionnaire)
+            <td>{{$questionnaire->name}}</td>
+            <td>{{$questionnaire->completed_times}}</td>
+            @endforeach
+        </tr>
+    </table>
+</x-app-layout>
