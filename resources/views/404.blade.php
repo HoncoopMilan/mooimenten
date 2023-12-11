d7384c02fe1fe95444c1e50f55c56eaa62747f1c
Anthony <93596000+AnthonyHartog@users.noreply.github.com>
2023-12-11 10:40:53 +0100

    Style + text van de pagina

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,3 +1,7 @@
 <x-app-layout>
-<h1>Test</h1>
+<div class="notFoundCenter">
+    <h1 class="E404">404 Not found</h1>
+    <h2 class="notFound">Pagina is niet gevonden</h2>
+    <p class="notFoundSure">Weet u zeker dat de link goed is getypt?</p>
+</div>
 </x-app-layout>
