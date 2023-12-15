6e7b3b40d7266cc20698fc2e559804d056f66d08
Anthony <93596000+AnthonyHartog@users.noreply.github.com>
2023-12-15 12:33:05 +0100

    En migration en model & migration en nav
    Company migrate en models etc. Nav aangemaakt (In commands)

--- database/migrations/2023_12_15_103210_create_companies.php
+++ database/migrations/2023_12_15_103210_create_companies.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('admin')->default(0);
-            $table->rememberToken();
+            $table->string('logo');
+            $table->string('color');
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('companies');
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             DeceasedSeeder::class,
             QuestionnaireSeeder::class,
             QuestionSeeder::class,
+            CompanySeeder::class,
             UserSeeder::class,
         ]);
 
