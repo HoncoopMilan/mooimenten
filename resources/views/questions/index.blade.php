c2007b2c07186710f5ec371395724aa60dffa584
HoncoopMilan <Honcoopmilan@gmail.com>
2023-12-15 12:34:27 +0100

    Css Bestaande vragen
    De css van de vragen die zijn aangemaakt en aangepast of verwijderd moeten worden

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -1,27 +1,49 @@
 <x-app-layout>
-    <h2>Vragen overzicht</h2>
-    <p>Om een vraag aan te maken zet waar de naam van de overledenen %name% neer, zodat de naam daar komt.</p>
-    <form style="margin: 0px;" action="{{ route('question.storeQuestion') }}" method="POST" enctype="multipart/form-data">
-        @csrf  
-        <input type="text" name="question" id="">
-        <button type="submit">Submit</button> 
-    </form> 
+    <div class="question-info">
+        <h2>Vragen overzicht</h2>
+        <p>Om een vraag aan te maken zet waar de naam van de overledenen %name% neer, zodat de naam daar komt.</p>
+    </div>
+    <div class="question-create">
+        <form style="margin: 0px;" action="{{ route('question.storeQuestion') }}" method="POST" enctype="multipart/form-data">
+            @csrf  
+            <input style="width: 600px" type="text" name="question" id="" placeholder="Typ hier uw vraag">
+            <div class="question-btn">
+                <button type="submit">Submit</button> 
+            </div>
+        </form>
+    </div>
 
-    <div class="form" style="margin-top: 20px;">
+    
+
+    <div class="form-question" style="margin-top: 20px;">
         @foreach($questions as $question)
         <div class="form-group" style="display:flex; align-items: center;">
-            <form style="margin-top: 3px; margin-bottom: 3px;" action="{{ route('question.update',$question->id) }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                @method('PUT')     
-                <input style="width: 300px;" type="text" name="question" id="" value="{{$question->question}}">
-                <button type="submit">Aanpassen</button> 
-            </form>   
-            <form style="margin-top: 3px; margin-bottom: 3px;" action="{{ route('question.destroy', $question->id) }}" method="Post">
-                @csrf
-                @method('DELETE')
-                <button style="margin-left: 5px;" type="submit">Verwijderen</button>
-            </form>
+            <div class="question">
+                <form style="margin-top: 3px; margin-bottom: 3px;" action="{{ route('question.update',$question->id) }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')     
+                    <div class="textfield">
+                        <input style="width: 600px;" type="text" name="question" id="" value="{{$question->question}}">
+                    </div>
+                <div class="question-buttons">
+                    <div class="question-btn">
+                        <button type="submit">Aanpassen</button> 
+                    </div>  
+                </form> 
+                <div class="question-btn">
+                    <form style="margin-top: 3px; margin-bottom: 3px;" action="{{ route('question.destroy', $question->id) }}" method="Post">
+                        @csrf
+                        @method('DELETE')
+                        <button style="margin-left: 5px;" type="submit">Verwijderen</button>
+                    </form>
+                </div>     
+                </div>
+                    
+                 
+                
+            </div>
+            
         </div>
         @endforeach
-    </div>
+    </div> 
 </x-app-layout>
