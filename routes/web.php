7a421f0a7ea20e4064013d0ea9837065f0951828
Anthony <93596000+AnthonyHartog@users.noreply.github.com>
2023-12-19 11:47:46 +0100

    Group routes created for companies
    Nu wat netter in een functie gezet zodat die gelijk alle company routes kan middlewaren

--- routes/web.php
+++ routes/web.php
@@ -52,8 +52,11 @@ Route::middleware(['auth', 'checkadmin'])->group(function () {
 
 Route::resource('questions', QuestionController::class);
 Route::resource('deceased', DeceasedController::class);
-Route::get('/companies/{companyName}', [CompanyController::class, 'edit'])->name('companie.edit')->middleware(['auth', 'checkadmin']);
-Route::resource('companies', CompanyController::class)->middleware(['auth', 'checkadmin']);
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/companies/{companyName}', [CompanyController::class, 'edit'])->name('companie.edit');
+    Route::resource('companies', CompanyController::class);
+});
 
 Route::post('/answer/check', [AnswerController::class, 'check'])->name('answer.check');
 Route::get('/answer/{customercode}', [AnswerController::class, 'show'])->name('answers.show');
