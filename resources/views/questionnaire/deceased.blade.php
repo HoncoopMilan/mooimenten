84269ccf350c5af32abcfe9362f2f62cdcc8fb72
GerbenVenneman <gerbenvenneman@gmail.com>
2023-12-21 13:47:38 +0100

    Deceased edit css
    Css toegepast op gegevens over de overledene aanpassen

--- resources/views/questionnaire/deceased.blade.php
+++ resources/views/questionnaire/deceased.blade.php
@@ -71,42 +71,42 @@
                         @csrf
                         @method('PUT')
                         <div class="form-input-questionnaire">
-                            <strong>Naam:</strong>
+                            <strong>Naam</strong>
                             <input type="text" name="name" placeholder="Naam" value="{{$deceased->name}}">
                             @error('name')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                             @enderror
                         </div>
                         <div class="form-input-questionnaire">
-                            <strong>Stad:</strong>
+                            <strong>Stad</strong>
                             <input type="text" name="city" placeholder="Stad" value="{{$deceased->city}}">
                             @error('city')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                             @enderror
                         </div>
                         <div class="form-input-questionnaire">
-                            <strong>Postcode:</strong>
+                            <strong>Postcode</strong>
                             <input type="text" name="zipcode" placeholder="Postcode" value="{{$deceased->zipcode}}">
                             @error('adress')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                             @enderror
                         </div>
                         <div class="form-input-questionnaire">
-                            <strong>Adres:</strong>
+                            <strong>Adres</strong>
                             <input type="text" name="adress" placeholder="Straat naam + Huisnummer" value="{{$deceased->adress}}">
                             @error('adress')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                             @enderror
                         </div>
                         <div class="form-input-questionnaire">
-                            <strong>Geboortedatum:</strong>
+                            <strong>Geboortedatum</strong>
                             <input type="date" name="date_of_birth" value="{{$deceased->date_of_birth}}">
                             @error('date_of_birth')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                             @enderror           
                         </div>
                         <div class="form-input-questionnaire">
-                            <strong>Overlijdensdatum:</strong>
+                            <strong>Overlijdensdatum</strong>
                             <input type="date" name="date_of_death" value="{{$deceased->date_of_death}}">
                             @error('date_of_death')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
@@ -119,16 +119,23 @@
                                 <a href="{{ route('deceased.destroyImg', $deceased->id) }}">Verwijder foto</a>         
                             </div>
                         @else
-                            <div class="form-input-questionnaire">
-                                <strong>Foto van hem:</strong>
+                            <Strong>Upload hier een foto van de overledene</Strong>
+                            <label class="custom-file-upload-deceased">
+                                <input name="img" type="file" multiple style="display:none" accept="image/*" onchange="checkImageCount(this)"/>
+                                <img id="img" src="{{asset('img/add-image.png')}}" alt="">
+                            </label>
+                            {{-- <div class="form-input-questionnaire">
+                                <strong>Upload hier een foto van de overledene</strong>
                                 <input type="file" name="img" multiple="true">
                                 @error('img')
                                 <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                                 @enderror
-                            </div>
+                            </div> --}}
                         @endif
                         <input type="hidden" name="questionnaire_id" value="{{$questionnaire->id}}">
-                        <button type="submit">Volgende</button>
+                        <div class="deceased-btn">
+                            <button type="submit">Volgende</button>
+                        </div>
                     </form>
                 @endif
             </div>
