9222fa2f7178720743de6de91ce500fd08fc270e
GerbenVenneman <gerbenvenneman@gmail.com>
2023-12-21 14:26:23 +0100

    Vragen kiezen css

--- resources/views/questionnaire/questions.blade.php
+++ resources/views/questionnaire/questions.blade.php
@@ -2,28 +2,36 @@
     <div class="questions-container">
         <div class="questions-sub-container">
             <div class="questions-sub-sub-container">
-                <h1>Maak uw vragenlijst</h1>
-                <p>Kies de vragen die u wenst terug te zien in de vragenlijst</p>
+                <h1 class="question-title">Kies de vragen die u wenst terug te zien in de vragenlijst</h1>
                 @if (\Session::has('error'))
                     <p style="color: red">{!! \Session::get('error') !!}</p>
                 @endif
                 @if(!isset($questionEntered))
                 <form class="formproject" action="{{ route('questions.store') }}" method="POST" enctype="multipart/form-data">
                     @csrf
+                    <div class="question-box">
                         @foreach ($questions as $question)
-                        <div class="form-group" >
-                            <div class="questions-box">
-                                <?php $questionQuestion = str_ireplace("%name%", $questionnaire->deceased->name, $question->question) ?>
-                                <input type="checkbox" id="" name="questions[{{$question->id}}]" value="{{$question->id}}">
-                                <label for="question{{$question->id}}">{{$questionQuestion}}</label>
+                            <div class="form-group" >
+                                
+                                    <?php $questionQuestion = str_ireplace("%name%", $questionnaire->deceased->name, $question->question) ?>
+                                    <input type="checkbox" id="" name="questions[{{$question->id}}]" value="{{$question->id}}">
+                                    <label for="question{{$question->id}}">{{$questionQuestion}}</label>
                             </div>
-                            
-                        </div>
                         @endforeach
+                    </div>
+                        
 
                     <input type="hidden" name="questionnaire_id" value="{{$questionnaire->id}}">
-                    <button type="submit" name="action" value="previous">Vorige</button>
-                    <button type="submit" name="action" value="next">Volgende</button>
+                    <div class="question-btn-flex">
+                        <div  class="deceased-btn">
+                            <button type="submit" name="action" value="previous">Vorige</button>
+                        </div>
+                        <div class="deceased-btn">
+                            <button type="submit" name="action" value="next">Volgende</button>
+                        </div>
+                    </div>
+                    
+                    
                 </form>
                 @else
                 <form class="formproject" action="{{ route('questions.update',$questionnaire->id) }}" method="POST" enctype="multipart/form-data">
