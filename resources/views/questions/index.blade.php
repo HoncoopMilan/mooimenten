fa3856d2bc193f0cd3a9888f41a1e108da3d580d
Anthony <93596000+AnthonyHartog@users.noreply.github.com>
2024-01-16 20:14:27 +0100

    Delete question function fixed
    Delete question function fixed

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -8,10 +8,8 @@
             <button class="questionCreate" style="margin-left: 5px;">Vraag aanmaken</button>
         </div>
     </div>
-    <div class="form-question">
-        <?php $count = 0;?>
+    <div class="form-question" style="padding-bottom: 50px;">
         @foreach($questions as $question)
-        <?php $count++ ?>
         <div class="form-group" style="display:flex; align-items: center;">
             <div class="question">
                 <form style="margin-top: 3px; margin-bottom: 3px;" action="{{ route('question.update',$question->id) }}" method="POST" enctype="multipart/form-data">
@@ -26,33 +24,20 @@
                     </div>  
                 </form> 
                 <div class="question-btn" style="margin-top: 3px; margin-bottom: 3px;">
-                    <form class="delete-{{$count}}" action="{{ route('question.destroy', $question->id) }}" method="Post">
+                    <form action="{{ route('question.destroy', $question->id) }}" method="Post">
                         @csrf
                         @method('DELETE')
+                        <button style="margin-right: 25px;" type="submit" onclick="return confirm('Als u de vraag verwijderd zal deze ook uit alle bestaande vragenlijsten worden verwijderd');">Verwijderen</button>
                     </form>
-                    <button class="submit-{{$count}}" style="margin-right: 25px;" type="submit">Verwijderen</button>
                 </div>     
                 </div>
             </div>
     </div>
-    <p id="count" style="display: none">{{$count}}</p>
         @endforeach
     <script>
         const count = document.querySelector('#count');
         const questionCreate = document.querySelector('.questionCreate');
         const questionForm = document.querySelector('.questionCreateForm');
-
-        for (let i = 1; i < (Number(count.textContent) + 1); i++) {
-            let submit = document.querySelector(`.submit-${i}`);
-            let deleteQuestion = document.querySelector(`.delete-${i}`);
-
-            submit.addEventListener('click', () => {
-                let sure = confirm('Als u de vraag verwijderd zal deze ook uit alle bestaande vragenlijsten worden verwijderd');
-                if(sure == true){
-                    deleteQuestion.submit();
-                }
-            });
-        }
 
         questionCreate.addEventListener('click', () => {
             let questionName = prompt('Naam van de vraag. Gebruik %name% voor de persoon zijn naam');
